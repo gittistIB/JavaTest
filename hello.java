8250e9282ec0f0137f4f98ed6fd315b18bd47245
gittistIB <oribs2002@yahoo.com>
2023-03-28 14:21:13 +0100

    3rd commit

--- hello.java
+++ hello.java
@@ -3,7 +3,8 @@ class hello{
   public static void main(String [] args){
 
       System.out.println("Hello DevOps class");
-      System.out.println("Hello friends")
+      System.out.println("Hello friends");
+      System.out.println("hello two");
 
 
 
