56dd04684cb25ffd31ce1ad2429c22effd44e746
gittistIB <oribs2002@yahoo.com>
2023-03-29 17:33:36 +0100

    adjust2

--- hello.java
+++ hello.java
@@ -5,8 +5,8 @@ class hello{
       System.out.println("Hello DevOps class");
       System.out.println("Hello friends");
       System.out.println("hello two");
-      System.out.println("Yeh I am enjoying CI tools for my DevOps training")
-      System.out.println("It is fun learning Jenkins CI tools")
+      System.out.println("Yeh I am enjoying CI tools for my DevOps training");
+      System.out.println("It is fun learning Jenkins CI tools");
 
 
 
